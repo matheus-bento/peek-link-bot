c61c01d1dd77509834b8a86b3097314a777700cd
Matheus Bento <matheus.bento@sinapseinformatica.com.br>
2021-08-12 22:11:34 -0300

    adding dom parsing with BeautifulSoup

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import praw
-import url
+from url import Url
 
 reddit = praw.Reddit('peek-link-bot')
 
 for item in reddit.inbox.stream():
     if item.type == 'username_mention' and not item.is_root:
         parent = reddit.comment(item.parent_id.split('_')[1])
-        urls = url.extract(parent.body)
+        urls = Url.extract(parent.body)
 
         for mentioned_url in urls:
-            print(mentioned_url)
+            Url.get_data(mentioned_url)
+            pass
+
--- url.py
+++ url.py
@@ -1,7 +1,21 @@
 import re
 
-def extract(comment):
-    url_re = re.compile(r'(?:https?:\/\/.)?(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b(?:[-a-zA-Z0-9@:%_\+.~#?&//=]*)')
+import requests
+from bs4 import BeautifulSoup
 
-    matches = url_re.findall(comment)
-    return matches
+class Url:
+
+    @staticmethod
+    def extract(comment):
+        url_re = re.compile(r'(?:https?:\/\/.)?(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b(?:[-a-zA-Z0-9@:%_\+.~#?&//=]*)')
+
+        matches = url_re.findall(comment)
+        return matches
+
+    def get_data(url):
+        html = requests.get(url).text
+        dom = BeautifulSoup(html, 'html.parser')
+
+        # TODO: add a switch to test if the given url is from a well-known
+        #       website in order to use its specific scrapper class
+        
