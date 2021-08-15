272380a68a3988972321b8c8248e4b8d85f97fbc
Matheus Bento <matheus.bento@sinapseinformatica.com.br>
2021-08-15 19:22:36 -0300

    change strings to double quotes

--- main.py
+++ main.py
@@ -2,15 +2,15 @@ import praw
 from peek_link_bot.url import Url
 
 def main():
-    reddit = praw.Reddit('peek-link-bot',)
+    reddit = praw.Reddit("peek-link-bot",)
 
     for item in reddit.inbox.stream():
-        if item.type == 'username_mention' and not item.is_root:
-            parent = reddit.comment(item.parent_id.split('_')[1])
+        if item.type == "username_mention" and not item.is_root:
+            parent = reddit.comment(item.parent_id.split("_")[1])
             urls = Url.extract(parent.body)
 
             for mentioned_url in urls:
-                print(Url(mentioned_url).get_data())
+                print(Url(mentioned_url).get_info())
                 pass
 
 if __name__ == "__main__":
--- tests/test_url_identification.py
+++ tests/test_url_identification.py
@@ -3,16 +3,13 @@ import peek_link_bot.url as url
 
 class TestURLIdentification(unittest.TestCase):
     def test_youtube_full_url(self):
-        self.assertTrue(url.YOUTUBE_URL_RE.match('https://www.youtube.com/watch?v=dQw4w9WgXcQ') != None)
+        self.assertTrue(url.YOUTUBE_URL_RE.match("https://www.youtube.com/watch?v=dQw4w9WgXcQ") != None)
 
     def test_youtube_short_url(self):
-        self.assertTrue(url.YOUTUBE_URL_RE.match('https://youtu.be/dQw4w9WgXcQ') != None)
+        self.assertTrue(url.YOUTUBE_URL_RE.match("https://youtu.be/dQw4w9WgXcQ") != None)
     
     def test_invalid_youtube_full_url(self):
-        self.assertTrue(url.YOUTUBE_URL_RE.match('https://ww.youtube.com/watch?v=dQw4w9WgXcQ') == None)
+        self.assertTrue(url.YOUTUBE_URL_RE.match("https://ww.youtube.com/watch?v=dQw4w9WgXcQ") == None)
     
     def test_invalid_youtube_short_url(self):
-        self.assertTrue(url.YOUTUBE_URL_RE.match('https://yout.ube/dQw4w9WgXcQ') == None)
-
-    def test_greater_than_2(self):
-        self.assertTrue(1 > 2)
+        self.assertTrue(url.YOUTUBE_URL_RE.match("https://yout.ube/dQw4w9WgXcQ") == None)
