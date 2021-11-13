33b29814b7f5ec00645ab63fd97ea84eef3b0720
Matheus Bento <matheus.bento@sinapseinformatica.com.br>
2021-11-12 22:46:40 -0300

    handling unread markers to handle which comments were already answered

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
-import os
-from datetime import datetime, timezone
-
+import os, sys
 import praw
-
 from peek_link_bot.url import Url
-from peek_link_bot.db.database import Database
-
-from peek_link_bot.db.model.comment import Comment
-from peek_link_bot.db.model.error import Error
 
 def main():
     reddit = praw.Reddit(
@@ -18,36 +11,19 @@ def main():
         password=os.getenv("REDDIT_PASSWORD")
     )
 
-    db = Database("sqlite:///peek_link_bot/db/comments.sqlite")
-    db.generate()
+    reddit.inbox.mark_unread(list(reddit.inbox.all()))
 
-    for item in reddit.inbox.stream():
-        if item.type == "username_mention" and not item.is_root:
-            parent = reddit.comment(item.parent_id.split("_")[1])
+    for notification in reddit.inbox.stream():
+        if notification.type == "username_mention" and not notification.is_root:
+            parent = reddit.comment(notification.parent_id.split("_")[1])
             urls = Url.extract(parent.body)
 
-            with db.get_session() as session:
-                for mentioned_url in urls:
-                    comment_replied = session.query(Comment).filter(Comment.comment_id==item.id).first() is not None
-                    comment_error = session.query(Error).filter(Error.comment_id==item.id).first() is not None
+            for mentioned_url in urls:
+                url_info = Url(mentioned_url).get_info()
+            
+                notification.reply(url_info)
+                notification.mark_read()
 
-                    if not comment_replied and not comment_error:
-                        try:
-                            url_info = Url(mentioned_url).get_info()
-                            item.reply(url_info)
-
-                            comment = Comment(comment_id=item.id,
-                                              created_utc=int(item.created_utc),
-                                              replied_utc=int(datetime.now(timezone.utc).timestamp()))
-
-                            session.add(comment)
-                        except Exception as exception:
-                            error = Error(comment_id=item.id,
-                                          message=str(exception),
-                                          error_utc=int(datetime.now(timezone.utc).timestamp()))
-
-                            session.add(error)
-                session.commit()
 
 
 if __name__ == "__main__":
--- peek_link_bot/db/model/comment.py
+++
@@ -1,9 +0,0 @@
-from peek_link_bot.db.model.base import Base
-from sqlalchemy import Column, Text, Integer
-
-class Comment(Base):
-    __tablename__ = 'comments'
-
-    comment_id = Column(Text, primary_key=True, nullable=False)
-    created_utc = Column(Integer, nullable=False)
-    replied_utc = Column(Integer)
